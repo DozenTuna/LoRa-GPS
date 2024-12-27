d3ea78a7045b3a02cd5f6955f2a498f276cd4a92
DozenTuna <86413592+DozenTuna@users.noreply.github.com>
2024-12-27 13:57:01 +0900

    create initial code to get attitude

--- board/LoRa-transmitter/src/main.cpp
+++ board/LoRa-transmitter/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_BNO055.h>
+#include <SPI.h>
 
 #define Addr_Pressure 0x76 //0x77 if jumper is connected, 0x76 if not
 #define Addr_Attitude 0x28 //0x29 if jumper(J1) is connected, 0x28 if not
@@ -27,20 +29,8 @@
 #define Batt_Voltage 26
 #define Button 23 //active low
 
-int i2c_read(int device_addr, unsigned int reg_addr){
-  Wire.beginTransmission(device_addr);
-  Wire.write(reg_addr);
-  Wire.endTransmission();
-  Wire.requestFrom(device_addr, 1);
-  return Wire.read();
-}
-
-void i2c_write(int device_addr, unsigned int reg_addr, unsigned int data){
-  Wire.beginTransmission(device_addr);
-  Wire.write(reg_addr);
-  Wire.write(data);
-  Wire.endTransmission();
-}
+Adafruit_BNO055 bno = Adafruit_BNO055(55, Addr_Attitude, &WireSensor);
+void get_bno055_data(void);
 
 void setup() {
   // put your setup code here, to run once:
@@ -60,23 +50,124 @@ void setup() {
   WireQT.setSCL(13);
   WireQT.begin();
   pinMode(LED_BUILTIN, OUTPUT);
+
+  if (!bno.begin()) // センサの初期化
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+  bno.setExtCrystalUse(false);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(LED_R, 255);
-  analogWrite(LED_G, 255);
-  analogWrite(LED_B, 255);
+  // analogWrite(LED_R, 255);
+  // analogWrite(LED_G, 255);
+  // analogWrite(LED_B, 255);
   // digitalWrite(LED_R, HIGH);
   // digitalWrite(LED_G, HIGH);
   // digitalWrite(LED_B, HIGH);
-  delay(1000);
-  analogWrite(LED_R, 230);
-  analogWrite(LED_G, 230);
-  analogWrite(LED_B, 230);
+  // delay(1000);
+  // analogWrite(LED_R, 230);
+  // analogWrite(LED_G, 230);
+  // analogWrite(LED_B, 230);
   // digitalWrite(LED_R, LOW);
   // digitalWrite(LED_G, LOW);
   // digitalWrite(LED_B, LOW);
+
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
+
+  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+  get_bno055_data();
   delay(1000);
   Serial.println("loop");
+}
+
+void get_bno055_data(void)
+{
+  // Possible vector values can be:
+  // - VECTOR_ACCELEROMETER - m/s^2
+  // - VECTOR_MAGNETOMETER  - uT
+  // - VECTOR_GYROSCOPE     - rad/s
+  // - VECTOR_EULER         - degrees
+  // - VECTOR_LINEARACCEL   - m/s^2
+  // - VECTOR_GRAVITY       - m/s^2
+  
+  
+  // キャリブレーションのステータスの取得と表示
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.print("CALIB Sys:");
+  Serial.print(system, DEC);
+  Serial.print(", Gy");
+  Serial.print(gyro, DEC);
+  Serial.print(", Ac");
+  Serial.print(accel, DEC);
+  Serial.print(", Mg");
+  Serial.print(mag, DEC);
+  Serial.println();
+  
+  
+  /*
+  // ジャイロセンサ値の取得と表示
+  imu::Vector<3> gyroscope = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  Serial.print(" 　Gy_xyz:");
+  Serial.print(gyroscope.x());
+  Serial.print(", ");
+  Serial.print(gyroscope.y());
+  Serial.print(", ");
+  Serial.print(gyroscope.z());
+  */
+  
+  /*
+  // 加速度センサ値の取得と表示
+  imu::Vector<3> accelermetor = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  Serial.print(" 　Ac_xyz:");
+  Serial.print(accelermetor.x());
+  Serial.print(", ");
+  Serial.print(accelermetor.y());
+  Serial.print(", ");
+  Serial.print(accelermetor.z());
+  */
+  
+  /*
+  // 磁力センサ値の取得と表示
+  imu::Vector<3> magnetmetor = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  Serial.print(" 　Mg_xyz:");
+  Serial.print(magnetmetor .x());
+  Serial.print(", ");
+  Serial.print(magnetmetor .y());
+  Serial.print(", ");
+  Serial.print(magnetmetor .z());
+  */
+
+  // センサフュージョンによる方向推定値の取得と表示
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  Serial.print("yaw:");
+  Serial.print(euler.x());
+  Serial.print(", roll:");
+  Serial.print(euler.y());
+  Serial.print(", pitch:");
+  Serial.print(euler.z());
+
+  /*
+    // センサフュージョンの方向推定値のクオータニオン
+    imu::Quaternion quat = bno.getQuat();
+    Serial.print("qW: ");
+    Serial.print(quat.w(), 4);
+    Serial.print(" qX: ");
+    Serial.print(quat.x(), 4);
+    Serial.print(" qY: ");
+    Serial.print(quat.y(), 4);
+    Serial.print(" qZ: ");
+    Serial.print(quat.z(), 4);
+    Serial.print("\t\t");
+  */
+
+  Serial.println();
 }
