aaf018313b7c157aaed610fd5e158146e7ff8aeb
DozenTuna <86413592+DozenTuna@users.noreply.github.com>
2024-12-31 18:26:04 +0900

    LoRa test done!

--- board/LoRa-transmitter/src/main.cpp
+++ board/LoRa-transmitter/src/main.cpp
@@ -16,7 +16,8 @@
 #define GPS_TxPin 4
 #define Sensor_SDA 2
 #define Sensor_SCL 3
-#define SerialLoRa Serial1
+// #define SerialLoRa Serial1
+SoftwareSerial SerialLoRa(LoRa_TxPin, LoRa_RxPin);
 #define SerialGPS Serial2
 #define WireSensor Wire1
 #define WireQT Wire
@@ -24,31 +25,55 @@
 #define LoRa_AUX 29
 #define LoRa_M0 28
 #define LoRa_M1 27
+int LoRa_Mode = 3;
 #define Batt_Voltage 26
 #define Button 23 //active low
 
-int i2c_read(int device_addr, unsigned int reg_addr){
-  Wire.beginTransmission(device_addr);
-  Wire.write(reg_addr);
-  Wire.endTransmission();
-  Wire.requestFrom(device_addr, 1);
-  return Wire.read();
+void setMode(int mode) {
+  if (LoRa_Mode != 3)
+  {
+    while (digitalRead(LoRa_AUX) == LOW)
+    {
+      delay(1);
+    }
+    delay(2);
+  }
+  switch (mode) {
+    case 0: //normal mode
+      digitalWrite(LoRa_M0, LOW);
+      digitalWrite(LoRa_M1, LOW);
+      break;
+    case 1: //wake up radio (transmit) mode
+      digitalWrite(LoRa_M0, HIGH);
+      digitalWrite(LoRa_M1, LOW);
+      break;
+    case 2: //wake on radio (receive) mode
+      digitalWrite(LoRa_M0, LOW);
+      digitalWrite(LoRa_M1, HIGH);
+      break;
+    case 3: //configuration / deep sleep mode
+      digitalWrite(LoRa_M0, HIGH);
+      digitalWrite(LoRa_M1, HIGH);
+      break;
+    default:
+      break;
+  }
+  LoRa_Mode = mode;
+  delay(1);
+  
 }
-
-void i2c_write(int device_addr, unsigned int reg_addr, unsigned int data){
-  Wire.beginTransmission(device_addr);
-  Wire.write(reg_addr);
-  Wire.write(data);
-  Wire.endTransmission();
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(1000);
   Serial.println("Hello World");
-  SerialLoRa.setRX(LoRa_RxPin);
-  SerialLoRa.setTX(LoRa_TxPin);
+  // pinMode(LoRa_RxPin, INPUT);
+  // pinMode(LoRa_TxPin, OUTPUT);
+  pinMode(LoRa_M0, OUTPUT);
+  pinMode(LoRa_M1, OUTPUT);
+  pinMode(LoRa_AUX, INPUT);
+  // SerialLoRa.setRX(LoRa_RxPin);
+  // SerialLoRa.setTX(LoRa_TxPin);
   SerialLoRa.begin(9600);
   SerialGPS.setRX(GPS_RxPin);
   SerialGPS.setTX(GPS_TxPin);
@@ -60,23 +85,16 @@ void setup() {
   WireQT.setSCL(13);
   WireQT.begin();
   pinMode(LED_BUILTIN, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite(LED_R, 255);
-  analogWrite(LED_G, 255);
-  analogWrite(LED_B, 255);
-  // digitalWrite(LED_R, HIGH);
-  // digitalWrite(LED_G, HIGH);
-  // digitalWrite(LED_B, HIGH);
-  delay(1000);
-  analogWrite(LED_R, 230);
-  analogWrite(LED_G, 230);
-  analogWrite(LED_B, 230);
-  // digitalWrite(LED_R, LOW);
-  // digitalWrite(LED_G, LOW);
-  // digitalWrite(LED_B, LOW);
-  delay(1000);
+  setMode(0);
+  int StartingAddr = 0x00;
+  SerialLoRa.write(StartingAddr);
+  SerialLoRa.write(StartingAddr);
+  SerialLoRa.write(StartingAddr);
+  SerialLoRa.write(0x70);
+  delay(10000);
   Serial.println("loop");
 }
